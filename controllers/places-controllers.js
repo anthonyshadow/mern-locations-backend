9bdf89993054d4a3ba77d4a934a5bf6e15a98633
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-22 11:40:47 -0400

    delete place by id mongodb

--- controllers/places-controllers.js
+++ controllers/places-controllers.js
@@ -6,6 +6,7 @@ const HttpError = require("../models/http-error");
 const getCoordsForAddress = require("../util/location");
 const Place = require("../models/place");
 const User = require("../models/user");
+const user = require("../models/user");
 
 let DUMMY_PLACES = [
   {
@@ -176,14 +177,24 @@ const deletePlace = async (req, res, next) => {
   let place;
 
   try {
-    place = await Place.findById(placeId);
+    place = await Place.findById(placeId).populate('creator');
   } catch (err) {
     const error = new HttpError("something went wrong", 500);
     return next(error);
   }
 
+  if (!place) {
+    const error = new HttpError('Could not find place for this id', 404);
+    return next(error)
+  }
+
   try {
-    await place.remove();
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await place.remove({session: sess});
+    place.creator.places.pull(place);
+    await place.creator.save({session: sess});
+    await sess.commitTransaction();
   } catch (err) {
     const error = new HttpError("something went wrong", 500);
     return next(error);
