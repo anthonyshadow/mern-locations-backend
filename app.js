e54295a8a1e676804c32321c6dcfc59703015ee6
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-24 19:22:43 -0400

    return static images:

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require('path')
 
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -12,6 +13,9 @@ const HttpError = require("./models/http-error");
 const app = express();
 
 app.use(bodyParser.json());
+
+//returns images statically
+app.use('/uploads/images', express.static(path.join('uploads', 'images')))
 
 //handling CORS errors
 app.use((req, res, next) => {
