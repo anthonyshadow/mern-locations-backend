2ab1a45faf82cddb398ba1abf22b7315b46de6e8
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-24 15:08:52 -0400

    fix missing parenthesis

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -20,8 +20,13 @@ router.post(
 );
 
 router.patch(
-  "/:pid",
-  [check("title").not().isEmpty, check("descriptiom").isLength({ min: 5 })],
+  '/:pid',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+    check('description').isLength({ min: 5 })
+  ],
   placesControllers.updatePlace
 );
 
