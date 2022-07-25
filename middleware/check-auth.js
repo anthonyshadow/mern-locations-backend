151fd9c8f744562c3b54c45c8f6b1147290f040a
Anthony shadowitz <anthonyshadowitz@Anthonys-MacBook-Pro.local>
2022-07-25 15:14:48 -0400

    add to check-auth

--- middleware/check-auth.js
+++ middleware/check-auth.js
@@ -9,13 +9,13 @@ module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1]; //Authorization: 'Bearer TOKEN' that is why we need to split on the space
     if (!token) {
-      throw new Error("Authentication Failed", 401);
+      throw new Error("Authentication Failed", 403);
     }
     const decodedToken = jwt.verify(token, "supersecret_dont_share");
     req.userData = { userId: decodedToken.userId };
     next();
   } catch (err) {
-    const error = new HttpError("Authentication failed", 401);
+    const error = new HttpError("Authentication failed", 403);
     return next(error);
   }
 };
